9b49c70181b7b3c23890d1d4086b2de02d0b946a
Aliaksei Semianiuk <seman17061984@gmail.com>
2024-01-20 09:35:05 +0300

    main: remove unused image settings

--- next.config.js
+++ next.config.js
@@ -1,16 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: 'export',
-  images: {
-    remotePatterns: [
-      {
-        protocol: 'https',
-        hostname: "apod.nasa.gov",
-        port: '',
-        pathname: '/apod/image/**',
-      },
-    ],
-  },
+  output: 'export',  
 }
 
 module.exports = nextConfig
