c97be104c891818c8951bdd7b69ab40d9e73de0b
Aliaksei Semianiuk <seman17061984@gmail.com>
2024-01-20 09:05:27 +0300

    main: remove errors from api

--- app/api/image/gallery/route.ts
+++ app/api/image/gallery/route.ts
@@ -2,15 +2,12 @@ import { getApodImagesForPeriod } from "@/service/fetchApodImage";
 import { NextResponse } from "next/server";
 
 export async function GET(req: Request): Promise<NextResponse> {
-        
-    const {searchParams} = new URL(req.url);
+
+    const { searchParams } = new URL(req.url);
     const startDate = searchParams.get('start') || '';
     const endDate = searchParams.get('end') || '';
 
-    try {
-        const galleryImages = await getApodImagesForPeriod(startDate, endDate);
-        return NextResponse.json(galleryImages);
-    } catch (error) {
-        return NextResponse.json({ error: "Something happened while download images" })
-    }
+    const galleryImages = await getApodImagesForPeriod(startDate, endDate);
+    return NextResponse.json(galleryImages);
+
 }
--- app/api/image/route.ts
+++ app/api/image/route.ts
@@ -3,13 +3,11 @@ import { NextResponse } from "next/server";
 
 export async function GET(req: Request): Promise<NextResponse> {
 
-    const {searchParams} = new URL(req.url);
+    const { searchParams } = new URL(req.url);
     const photoDate = searchParams.get('photoDate') || '';
 
-    try {
-        const imageForDate = await getApodImage(photoDate);
-        return NextResponse.json(imageForDate);
-    } catch (error) {
-        return NextResponse.json({ error: "Something happened while download images" })
-    }
+
+    const imageForDate = await getApodImage(photoDate);
+    return NextResponse.json(imageForDate);
+
 }
